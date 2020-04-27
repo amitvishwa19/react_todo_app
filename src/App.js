d41af44c0b106b050c44bdfbcd9abcc86739cba4
jaysvishwagit <jaysvishwa@gmail.com>
2020-04-27 18:25:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+import Header from './components/layout/Header';
+import Todos from './components/todo/Todos';
+import AddTodo from './components/todo/AddTodo'
+import About from './components/pages/About'
+import {v1 as uuid} from "uuid";
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component{
+  state = {
+     todos:[
+       {
+         id:uuid(),
+         title:'Put garbage to the trash',
+         completed:false
+       },
+       {
+        id:uuid(),
+        title:'Wash the car',
+        completed:false
+      },
+      {
+        id:uuid(),
+        title:'Meeting with boss',
+        completed:false
+      }
+     ]
+  }
+
+  //Toggle todo completed
+  markComplete = (id) =>{
+    this.setState({todos:this.state.todos.map(todo=>{
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+
+  delTodo = (id) =>{
+    this.setState({todos:[...this.state.todos.filter(todo =>todo.id !==id)]})
+  }
+
+  addTodo = (title) =>{
+    const newTodo ={
+      id:uuid(),
+      title:title,
+      completed:false
+    }
+    this.setState({todos: [...this.state.todos,newTodo]})
+  }
+
+  render(){
+    //console.log(this.state.todos)
+    return(
+      <Router>
+        <div className="App">
+
+          <Header/>
+
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo}/>
+              <Todos todos = {this.state.todos}  markComplete={this.markComplete} delTodo={this.delTodo}/>
+            </React.Fragment>
+          )}/>
+
+          <Route path="/about" component={About}/>
+        
+        </div>
+      </Router>
+    );
+  }
+}
 export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is a sample react app v1.0.0.1</p>
+        </div>
+    )
+}
